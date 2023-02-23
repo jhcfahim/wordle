3765c24fb6de06d046b9bec2cd468cb3a1ef2ba8
Fahim Firdaus <108272834+jhcfahim@users.noreply.github.com>
2023-02-23 21:36:54 +0000

    added random and pathlib, to get a random word each time, also says misplaced and wrong letters

--- main.py
+++ main.py
@@ -1,13 +1,32 @@
-WORD = "SNAKE"
+import pathlib
+import random
 
-for guess_num in range(1, 7):
+WORDLIST = pathlib.Path("wordlist.txt")
+
+words = [
+  word.upper()
+  for word in WORDLIST.read_text(encoding="utf-8").strip().split("\n")
+]
+word = random.choice(words)
+
+print("Testing secret word: ", word)
+
+for guess_num in range(1, 6):
   guess = input(f"\nGuess {guess_num}: ").upper()
 
-  if guess == "WORD":
+  if guess == word:
     print("Correct")
-    break # Ends the wordle when the user guesses the word.
+    break  # Ends the wordle when the user guesses the word.
   else:
     print("Wrong")
 
-    comprehension_set = {letter for letter, correct in zip(guess, WORD) if letter == correct}
-    print(comprehension_set)
+    correct_letters = {
+      letter
+      for letter, correct in zip(guess, word) if letter == correct
+    }
+    misplaced_letters = set(guess) & set(word) - correct_letters
+    wrong_letters = set(guess) - set(word)
+
+    print("Correct letters:", ", ".join(sorted(correct_letters)))
+    print("Misplaced letters:", ", ".join(sorted(misplaced_letters)))
+    print("Wrong letters:", ", ".join(sorted(wrong_letters)))
