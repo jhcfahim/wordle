6b73285e8e03ada36719441cde13bc811747f99d
Fahim Firdaus <108272834+jhcfahim@users.noreply.github.com>
2023-02-23 21:06:18 +0000

    I added user input of guessed word.

--- main.py
+++ main.py
@@ -1,8 +1,2 @@
-def print_menu():
-  print("Let's play Wordle:")
-  print("Type a five letter word and hit enter!")
-
-def read_random_word():
-  
-
-print_menu()
+guess = input("Guess a word: ")
+print(guess)
