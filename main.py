de5ccacc522276502800dd7d4a64a07294f2ec1e
Fahim Firdaus <108272834+jhcfahim@users.noreply.github.com>
2023-02-23 21:20:09 +0000

    said if guess is wrong or not, and checked what letters are correct

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
-guess = input("Guess a word: ")
-print(guess)
+WORD = "SNAKE"
+
+for guess_num in range(1, 7):
+  guess = input(f"\nGuess {guess_num}: ").upper()
+
+  if guess == "WORD":
+    print("Correct")
+    break # Ends the wordle when the user guesses the word.
+  else:
+    print("Wrong")
+
+    comprehension_set = {letter for letter, correct in zip(guess, WORD) if letter == correct}
+    print(comprehension_set)
