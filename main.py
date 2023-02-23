bcde226fd68b7ff93f9cafaeb92e19c31fd02b98
Fahim Firdaus <108272834+jhcfahim@users.noreply.github.com>
2023-02-23 21:40:18 +0000

    if the user did not guess the word, then the program tells them the word

--- main.py
+++ main.py
@@ -9,17 +9,18 @@ words = [
 ]
 word = random.choice(words)
 
-print("Testing secret word: ", word)
+print("Testing secret word: ", word) # testing purposes only, delete later
 
-for guess_num in range(1, 6):
+for guess_num in range(1, 6): # gives the user 5 guesses
   guess = input(f"\nGuess {guess_num}: ").upper()
 
   if guess == word:
     print("Correct")
-    break  # Ends the wordle when the user guesses the word.
+    break  # ends the wordle when the user guesses the word.
   else:
     print("Wrong")
 
+    # tells the user which letters are correct, misplaced, and wrong
     correct_letters = {
       letter
       for letter, correct in zip(guess, word) if letter == correct
@@ -30,3 +31,6 @@ for guess_num in range(1, 6):
     print("Correct letters:", ", ".join(sorted(correct_letters)))
     print("Misplaced letters:", ", ".join(sorted(misplaced_letters)))
     print("Wrong letters:", ", ".join(sorted(wrong_letters)))
+
+else:
+  print(f"The word was {word}")
